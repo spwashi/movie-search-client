dfaf0cfb4a3c30248db597e3bdeaa03432153cb9
Sam Washington <sam@spwashi.com>
2022-06-18 19:39:28 -0500

    logo update

--- src/app/components/header/AppHeader.tsx
+++ src/app/components/header/AppHeader.tsx
@@ -1,4 +1,3 @@
-import logo from '../../../assets/ABR-Logo-Horizontal.png';
 import React from 'react';
 import {MovieSearchBar} from '../../features/movie-search/components/search/MovieSearchBar';
 import styled from 'styled-components';
@@ -12,8 +11,8 @@ const HeaderWrapper =
 export function AppHeader() {
   return (
     <HeaderWrapper>
-      <a href="https://theabr.org" title="The American Board of Radiology">
-        <img src={logo} className="logo" alt="ABR logo"/>
+      <a href="/">
+        <img src="/logo512.png" className="logo" alt="logo"/>
       </a>
       <AccessibilityMenu/>
       <MovieSearchBar location="header"/>
